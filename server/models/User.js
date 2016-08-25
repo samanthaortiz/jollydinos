92e890ce738b49ab9c410ecaa0db0ec7230b6c13
Yueh Chou <yueh.k.chou@gmail.com>
2016-08-25 10:46:26 -0400

    Turn schema.js into User.js and Job.js

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+var db = require("./db")
+
+var UserSchema = new db.Schema({
+  username: {
+    type: String,
+    "username": String,
+    required: true,
+    unique: true,
+    "password": String
+  }
+});
+
+module.exports = db.model('users', UserSchema);
