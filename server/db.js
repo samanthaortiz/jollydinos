88b1fa828ccc052b362d84dd0f305470f73dc16d
Jack <jisapp02@gmail.com>
2016-08-25 16:21:44 -0400

    Router fixes

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 /* Use local url if not in production -- mlab*/
-var dbURL = process.env.MONGODB_URI|| "mongodb://localhost/workshop";
+var dbURL = process.env.MONGODB_URI || "mongodb://localhost/githired";
 
 /* Connect and handle errors */
 module.exports = mongoose.connect(dbURL, function(err) {
