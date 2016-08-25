84da64de8007443b47330ce7e8ed7d657cfe4aa3
ellier7 <eroussopoulos@gmail.com>
2016-08-25 17:28:40 -0400

    waaah

--- server/models/Job.js
+++ server/models/Job.js
@@ -1,14 +1,12 @@
 var db = require("../db")
 
 var JobSchema = new db.Schema({
-  jobs: {
-    type: String,
-    required: true,
-    company: String,
-    position: String,
-    deadline: Date,
-    status: String
-  }
+    'type': String,
+    // required: true,
+    'company': String,
+    'position': String,
+    'deadline': Date,
+    'status': String
 });
 
-  module.exports = db.model('jobs', JobSchema);
+  module.exports = db.model('Job', JobSchema);
