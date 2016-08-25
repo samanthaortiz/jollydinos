e7f25ee465ecce1238c59b33f6af10e7ad1e20c4
ellier7 <eroussopoulos@gmail.com>
2016-08-25 11:18:29 -0400

    idk

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 var mongoose = require("mongoose");
 
 /* Use local url if not in production -- mlab*/
-var dbURL = process.env.DBURL || process.env.MONGODB_URI || "mongodb://localhost/workshop";
+var dbURL = process.env.MONGODB_URI|| "mongodb://localhost/workshop";
 
 /* Connect and handle errors */
 module.exports = mongoose.connect(dbURL, function(err) {
-  if (err) console.error(err);
-  else console.log("Successfully connected to database");
+  if (err) console.error("errrorrr", err);
+  else console.log("Successfully connected to database", dbURL);
 });
