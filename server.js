a279e7005878b01113eac118f90a12796c5a6348
Jack <jisapp02@gmail.com>
2016-08-25 16:21:44 -0400

    Router fixes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var apiRouter = require('./server/router.js');
 
 var app = express();
 
 app.use("/", express.static(__dirname + "/client/"));
+
+app.use("/api", apiRouter);
 
 
 app.listen(process.env.PORT || 3000);
--- server/router.js
+++ server/router.js
@@ -8,17 +8,12 @@ var Job = require('./models/Job');
 
 
 router.get('/listing', function(req, res){
-	Job.find({
-	"type": req.body.type,
-    "required": req.body.required,
-    "company": req.body.company,
-    "position": req.body.position,
-    "deadline": req.body.deadline,
-    "status": req.body.status
-	})
-	save(function(err, task){
-    res.status(201).send(task)
+	Job.find(function(err, doc){
+    console.log('REQ BODY', req.body);
+    console.log('DOC', doc);
+    res.json(doc);
   });
+
 });
 
 
