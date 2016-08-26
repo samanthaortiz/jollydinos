c8d9b61700c9b36d212f81cfc972fd6bfd99efa7
Jack <jisapp02@gmail.com>
2016-08-26 14:44:02 -0400

    index.html linked to view, but not populating

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 angular.module('gitHired', [
   // Other modules that will provide controllers and services.
   'gitHired.listing',
+  'gitHired.services',
    // Angular plugin that provides $routeProvider and the $routeChangeStart event.
   'ngRoute'
 ])
@@ -11,9 +12,10 @@ angular.module('gitHired', [
     // Controllers are automatically connected to the Templates
     // A single controller can be used to control multiple views
     // Beware nested controllers!
-    templateUrl: 'client/listing/listing.html',
+    templateUrl: 'listing/listing.html',
     controller: 'JobsController'
   })
   .otherwise({
     redirectTo: '/listing'
   });
+});
