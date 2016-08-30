223206d9600b828ba9bb2e644c91de33a43fbdd5
ellier7 <eroussopoulos@gmail.com>
2016-08-30 09:18:35 -0400

    Login in progress

--- client/auth/auth.js
+++
@@ -1,5 +0,0 @@
-// angular.module('gitHired.auth', [])
-// .controller('LoginController', function ($scope, $window, $location))
-
-// .controller('AuthController', function ($scope, $window, $location, Auth) {
-
