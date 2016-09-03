1561c764d98567178ba388336b7232373045ceca
Jack <jisapp02@gmail.com>
2016-09-02 23:09:52 -0400

    Progress bar now sorts correctly after edits

--- server/router.js
+++ server/router.js
@@ -3,6 +3,17 @@ var router = require('express').Router();
 //Import models
 var User = require('./models/User');
 var Job = require('./models/Job');
+var orders = {
+  'Interested': 0,
+  'Outreach': 1,
+  'Phone Interview': 2,
+  'Coding Challenge': 3,
+  'Onsite Interview': 4,
+  'Offer Received': 5,
+  'Employer Declined': 6,
+  'Offer Accepted': 6,
+  'Offer Declined': 6
+};
 
 router.get('/listing', function(req, res){
 	Job.find(function(err, doc){
@@ -12,17 +23,6 @@ router.get('/listing', function(req, res){
 });
 
 router.post("/listing", function(req, res) {
-  var orders = {
-    'Interested': 0,
-    'Outreach': 1,
-    'Phone Interview': 2,
-    'Coding Challenge': 3,
-    'Onsite Interview': 4,
-    'Offer Received': 5,
-    'Employer Declined': 6,
-    'Offer Accepted': 6,
-    'Offer Declined': 6
-  };
 
   new Job ({
     'type': req.body.type,
@@ -47,6 +47,7 @@ router.delete("/listing", function(req, res) {
 });
 
 router.put("/listing", function(req, res) {
+  req.body.statusOrder = orders[req.body.status];
   Job.findByIdAndUpdate(req.body._id, req.body, function (err) {
     if (err) throw err;
     res.send(req.body);
