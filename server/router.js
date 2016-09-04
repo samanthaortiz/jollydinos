826d9e77686fac9238e20284db55857ca8fac0bd
Yueh Chou <yueh.k.chou@gmail.com>
2016-09-04 18:14:55 -0400

    sort by added date

--- server/router.js
+++ server/router.js
@@ -62,6 +62,7 @@ router.delete("/listing", function(req, res) {
 });
 
 router.put("/listing", function(req, res) {
+
   req.body.statusOrder = orders[req.body.status];
   Job.findByIdAndUpdate(req.body._id, req.body, function (err) {
     if (err) throw err;
