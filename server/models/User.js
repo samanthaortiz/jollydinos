df0b4ab68fb6d8259ab1c2b81600c7115d3f1467
Jack <jisapp02@gmail.com>
2016-09-06 17:36:30 -0400

    Code consistency and cleanup

--- server/models/User.js
+++ server/models/User.js
@@ -2,21 +2,21 @@ var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 
 var users = mongoose.Schema({
-   local            : {
-        email        : String,
-        password     : String,
-   		name 		 : String
-    }
+  local: {
+    email: String,
+    password: String,
+ 		name: String
+  }
 });
 
 
 users.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
 };
 
 // checking if password is valid
 users.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+  return bcrypt.compareSync(password, this.local.password);
 };
 
 
