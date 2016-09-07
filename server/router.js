f33246fda73106430586bad3705371c3447acb5f
selimc <selimcebecioglu@gmail.com>
2016-09-07 16:56:25 -0400

    refactoring

--- server/router.js
+++ server/router.js
@@ -1,62 +1,9 @@
 var router = require('express').Router();
+var jobController = require('./models/jobController.js')
 
-//Import models
-var User = require('./models/User.js');
-var Job = require('./models/Job.js');
-// var util = require('./util')
-
-
-var orders = { //If adding or removing labels, be sure to edit max value of adjustStatus in listing controller
-  'Interested': 0,
-  'Outreach': 1,
-  'Phone Interview': 2,
-  'Coding Challenge': 3,
-  'Onsite Interview': 4,
-  'Offer Received': 5,
-  'Employer Declined': 6,
-  'Offer Declined': 7,
-  'Offer Accepted': 8
-};
-
-router.get('/listing', function(req, res){
-  Job.find({})
-  .where('email').equals('test@test.com')
-  .exec(function(err, doc){
-    res.json(doc);
-  });
-});
-
-router.post("/listing", function(req, res) {
-  new Job ({
-    'email': 'test@test.com',
-    'type': req.body.type,
-    'company': req.body.company,
-    'position': req.body.position,
-    'deadline': req.body.deadline,
-    'link': req.body.link,
-    'status': req.body.status,
-    'statusOrder': orders[req.body.status],
-    'modifiedAt': new Date()
-  })
-  .save(function(err, task){
-    res.status(201).json(task)
-  });
-});
-
-router.delete("/listing", function(req, res) {
-  Job.findByIdAndRemove(req.body._id, function (err) {
-    if (err) throw err;
-    res.send(req.body);
-  });
-});
-
-router.put("/listing", function(req, res) {
-  req.body.statusOrder = orders[req.body.status];
-  Job.findByIdAndUpdate(req.body._id, req.body, function (err) {
-    if (err) throw err;
-    res.send(req.body);
-  });
-});
-
+router.get('/listing', jobController.getAll);
+router.post("/listing", jobController.addOne);
+router.delete("/listing", jobController.deleteOne);
+router.put("/listing", jobController.updateOne);
 
 module.exports = router;
